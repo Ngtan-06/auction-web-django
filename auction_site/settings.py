45faf622a08e713522dc652ebefa6c5d51733e9f
Ngtan-06 <tungoctan999@gmail.com>
2026-06-22 14:33:28 +0700

    login2

--- auction_site/settings.py
+++ auction_site/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,9 +82,13 @@ WSGI_APPLICATION = 'auction_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': os.getenv('SUPABASE_PASSWORD'),
+            'HOST': os.getenv('SUPABASE_HOST'),
+            'PORT': '5432',
+        }
 }
 
 
