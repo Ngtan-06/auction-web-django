c2fdff5f723ef902ad4f1471962e257da1c5712d
Ngtan-06 <tungoctan999@gmail.com>
2026-07-02 16:21:04 +0700

    2

--- auction_site/settings.py
+++ auction_site/settings.py
@@ -75,7 +75,10 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL')]
+            "hosts": [{
+                "address": os.getenv("REDIS_URL"),
+                "ssl": True,
+            }],
         },
     },
 }
