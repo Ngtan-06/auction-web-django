dc429e44ebdbb08d31b0926c42fd000b863565b7
Ngtan-06 <tungoctan999@gmail.com>
2026-07-07 18:32:51 +0700

    31

--- auctions/apps.py
+++ auctions/apps.py
@@ -1,11 +1,4 @@
 from django.apps import AppConfig
 
-
 class AuctionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'auctions'
-
-    def ready(self):
-        # Gọi trình quét ngầm hoạt động khi ứng dụng sẵn sàng
-        from . import updater
-        updater.start()
